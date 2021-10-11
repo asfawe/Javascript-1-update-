1d41c2ce59c90c2bbcd319b9c9dd41dc051327b9
unknown <aronia5137@gmail.com>
2021-10-11 23:23:35 +0900

    first commit

---
+++ app5.js
@@ -0,0 +1,28 @@
+// or and || && 연산자 연습 시간
+
+// // ||
+// true || true === true
+// false || true === true
+// true || false === true
+// false || false === false
+
+// // &&
+// true && true === true
+// false && true === false
+// true && false === false
+// false && false === false
+
+// 계산기.
+const age = parseInt(prompt("How old are you?"));
+
+if(isNaN(age) || age < 0){
+    console.log("Please write a real positive number");
+} else if (age < 18){
+    console.log("You are too young.");
+} else if(age >= 18 && age <= 50){
+    console.log("You can drink");
+} else if(age > 50 && age <= 80){
+    console.log("You should exercise");
+} else if(age > 80){
+    console.log("You can do whatever you want.");
+}
